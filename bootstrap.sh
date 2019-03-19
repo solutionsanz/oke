4c40cf18bcf0e39c1c6c6082b74449418478e578
SolutionsANZ <solutionsanz2@gmail.com>
2019-03-19 16:25:39 +1100

    Adding Vagrant file

--- bootstrap.sh
+++ bootstrap.sh
@@ -43,6 +43,14 @@ INSTALL_KUBECTL_BOOLEAN=$6
     sudo apt-get install git -y
    
     echo "##########################################################################"
+    echo "############# Installing and configuring Terraform #######################"
+
+    mkdir /home/vagrant/terraform && cd /home/vagrant/terraform
+    wget https://releases.hashicorp.com/terraform/0.11.11/terraform_0.11.11_linux_amd64.zip
+	sudo apt-get install unzip -y && sudo unzip /home/vagrant/terraform/terraform_*.zip -d /usr/local/bin
+	terraform --version
+
+    echo "##########################################################################"
     echo "############### Installing NodeJS on an Ubuntu Machine ###############"
 
     sudo curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
